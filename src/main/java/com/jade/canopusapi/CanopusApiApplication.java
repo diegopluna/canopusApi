72e7b0a19cc322637dc9267e039538b4cc64a99e
Diego Peter <dpl@cesar.school>
2023-11-04 23:11:54 -0300

    Hello!

---
+++ src/main/java/com/jade/canopusapi/CanopusApiApplication.java
@@ -0,0 +1,20 @@
+package com.jade.canopusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CanopusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanopusApiApplication.class, args);
+	}
+
+	@GetMapping("/api")
+	public String hello() {
+		return "Hello World";
+	}
+}
