15d165604ada77b1a3314d9295e99a143a996f58
diegoluna <dpl@cesar.school>
2023-11-08 10:15:17 -0300

    non sockjs connection

--- src/main/java/com/jade/canopusapi/config/WebSocketConfig.java
+++ src/main/java/com/jade/canopusapi/config/WebSocketConfig.java
@@ -1,5 +1,4 @@
 package com.jade.canopusapi.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
@@ -9,6 +8,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*");
         registry.addEndpoint("/ws").withSockJS();
     }
 
